7b788e8e7ecd6033dadd356cf23de37771f4d142
Gabriel Suarez <gabriel.suarez@solarwinds.com>
2020-01-21 21:40:10 +0000

    Added new line

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,6 +5,7 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+
 def test_cron_service(host):
     os = host.system_info.distribution
     if os in ("ubuntu", "debian"):
