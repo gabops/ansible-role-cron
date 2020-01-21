dd7463f15af170c73c66dbf334178f4fefe31c9b
Gabriel Suarez <gabriel.suarez@solarwinds.com>
2020-01-21 21:22:29 +0000

    the default scenario will only run test with the defaults to be sure nothing breaks with the default role values

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,16 +5,6 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-
-def test_cron_jobs(host):
-    c = host.run('crontab -l')
-
-    assert '*/3 * * * * touch /tmp/cron_job' in c.stdout
-    assert '*/3 * * * * touch /tmp/common_job' in c.stdout
-    assert '*/3 * * * * touch /tmp/group_job' in c.stdout
-    assert '*/3 * * * * touch /tmp/host_job' in c.stdout
-
-
 def test_cron_service(host):
     os = host.system_info.distribution
     if os in ("ubuntu", "debian"):
